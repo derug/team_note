ef4f74d3d354189c9b8cd445040144be8b906bd3
derug <78718976+derug@users.noreply.github.com>
2023-02-24 15:32:14 +0900

    Update InverseModular.cpp

--- math/InverseModular.cpp
+++ math/InverseModular.cpp
@@ -1,9 +1,9 @@
-const long long MAX, MOD;
-long long iv[MAX];
+const int MAX, MOD;
+long long iv[MAX+1];
 
 void init(){
   iv[1] = 1;
   for(int i=2; i<MAX; i++){
-    iv[i]=MOD-1LL*(MOD/i)*iv[MOD%i]%MOD;
+    iv[i]=MOD-(MOD/i)*iv[MOD%i]%MOD;
   }
 }
