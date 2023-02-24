f6313c255d6b26fabe19497fbdd2353dbe4cb038
derug <78718976+derug@users.noreply.github.com>
2023-02-24 17:09:53 +0900

    Update Fast_Find_Prime.cpp

--- math/Fast_Find_Prime.cpp
+++ math/Fast_Find_Prime.cpp
@@ -1,22 +1,21 @@
-int a[5]={2, 3, 5, 7, 11};
+typedef unsigned long long ull;
 
-unsigned long long sq(unsigned long long n, unsigned long long r, unsigned long long p){
-    if(r==1) return n;
+ull sq(ull n, ull r, ull p){
+    if(r==0) return 1;
+    if(r&1) return n*sq(n,r-1,p)%p;
     ull s=sq(n, r/2, p);
-    s=s*s%p;
-    if(r%2) return n*s%p;
-    return s;
+    return s*s%p;
 }
 
-bool check(ull n){
+bool is_prime(ull n){
+    if(n==2) return true;
+    if(n%2) return false;
     ull d=n-1, r=0;
-    while(d%2==0){
-        d/=2;
-        r++;
-    }
+    while(d%2==0){d/=2; r++;}
+    
+    int a[5]={2, 3, 5, 7, 11};
     for(int k=0; k<5; k++){
         if(a[k]==n) return true;
-
         ull s=sq(a[k], d, n);
         if(s==1||s==n-1) continue;
         for(int i=1; i<r; i++){
@@ -27,3 +26,5 @@ bool check(ull n){
     }
     return true;
 }
+
+//폴라드 로 추가 예정
