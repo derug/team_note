013a42a0fd9e42cbe9ff9580325ef768f45223f6
derug <78718976+derug@users.noreply.github.com>
2023-02-24 18:00:54 +0900

    Update SegmentTree.cpp

--- data-structure/SegmentTree.cpp
+++ data-structure/SegmentTree.cpp
@@ -4,14 +4,14 @@ int arr[MAX], sgt[MAX*4];
 int init(int l, int r, int node){
   if(l==r) return sgt[node]=arr[l];
   int mid=l+r>>2;
-  return seg[node]=min(init(l, mid, node*2), init(mid+1, r, node*2+1));
+  return sgt[node]=min(init(l, mid, node*2), init(mid+1, r, node*2+1));
 }
 
 int update(int idx, int val, int nl, int nr, int node){
-  if(idx<nl||nr<idx) return seg[node];
-  if(nl==nr) return seg[node]=min(seg[node], val);
+  if(idx<nl||nr<idx) return sgt[node];
+  if(nl==nr) return sgt[node]=min(sgt[node], val);
   int mid=nl+nr>>1;
-  return seg[node]=min(update(idx,val,nl,mid,2*node), update(idx,val,mid+1,nr,2*node+1));
+  return sgt[node]=min(update(idx,val,nl,mid,2*node), update(idx,val,mid+1,nr,2*node+1));
 }
 
 int query(int l, int r, int nl, int nr, int node){
