799e8a56669a7dad2521c823460c9e68555463e7
derug <78718976+derug@users.noreply.github.com>
2023-02-24 15:39:16 +0900

    Update and rename InverseModular.cpp to Fast_Power&Modular_Inverse.cpp

---
+++ math/Fast_Power&Modular_Inverse.cpp
@@ -0,0 +1,13 @@
+const int MOD;
+
+long long fastPow(long long a, long long b){
+  if(b==0) return 1;
+  if(b&1) return a*fastPow(a,b-1)%MOD;
+  long long tmp=fastPow(a,b/2);
+  return tmp*tmp%MOD;
+}
+
+inline long long divMod(long long a, long long b){
+  b=fastPow(a, MOD-2);
+  return a*b%MOD;
+}
--- math/InverseModular.cpp
+++
@@ -1,9 +0,0 @@
-const int MAX, MOD;
-long long iv[MAX+1];
-
-void init(){
-  iv[1] = 1;
-  for(int i=2; i<MAX; i++){
-    iv[i]=MOD-(MOD/i)*iv[MOD%i]%MOD;
-  }
-}
