908e2c58ed3e5d271dc61ec786549f0f8208d036
derug <78718976+derug@users.noreply.github.com>
2023-02-28 19:29:27 +0900

    Update Fast_Find_Prime.cpp

--- math/Fast_Find_Prime.cpp
+++ math/Fast_Find_Prime.cpp
@@ -1,29 +1,56 @@
 typedef unsigned long long ull;
 
-ull sq(ull n, ull r, ull p){
-    if(r==0) return 1;
-    if(r&1) return n*sq(n,r-1,p)%p;
-    ull s=sq(n, r/2, p);
-    return s*s%p;
+typedef unsigned long long ull;
+
+inline ull addmod(ull a, ull b, ull p){
+    a%=p, b%=p;
+    return (a>=p-b?a-(p-b):a+b);
+}
+
+inline ull mulmod(ull a, ull b, ull p){
+    a%=p, b%=p;
+    ull r=0;
+    while(b){
+        if(b&1) r=addmod(r, a, p);
+        a=addmod(a, a, p);
+        b>>=1;
+    }
+    return r;
+}
+
+inline ull powmod(ull a, ull b, ull p){
+    a%=p;
+    ull r=1;
+    while(b){
+        if(b&1) r=mulmod(r, a, p);
+        a=mulmod(a, a, p);
+        b>>=1;
+    }
+    return r;
+}
+
+bool miller_rabin(ull a, ull n, ull r){
+    if(a>=n) a%=n;
+    if(a<=1) return true;
+    ull d=n>>r, s=powmod(a, d, n);
+    if(s==1||s==n-1) return true;
+    while(--r){
+        s=powmod(s, 2, n);
+        if(s==1) return false;
+        if(s==n-1) return true;
+    }
+    return false;
 }
 
 bool is_prime(ull n){
     if(n==2) return true;
-    if(n%2) return false;
-    ull d=n-1, r=0;
-    while(d%2==0){d/=2; r++;}
-    int a[5]={2, 3, 5, 7, 11};
-    for(int k=0; k<5; k++){
-        if(a[k]==n) return true;
-        ull s=sq(a[k], d, n);
-        if(s==1||s==n-1) continue;
-        for(int i=1; i<r; i++){
-            s=sq(s, 2, n);
-            if(s==n-1) break;
-        }
-        if(s!=n-1) return false;
-    }
-    return true;
+    if(n<2||n%2==0) return false;
+    ull d=n>>1, r=1;
+    while((d&1)==0){d>>=1; r++;}
+#define T(a) miller_rabin(a##ull, n, r)
+    if(n<4759123141ull) return T(2)&&T(7)&&T(61);
+    return T(2)&&T(325)&&T(9375)&&T(28178)&&T(450775)&&T(9780504)&&T(1795265022);
+#undef T
 }
 
 //폴라드 로 추가 예정
