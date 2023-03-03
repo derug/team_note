aca0c91990260e5111091488664e0a1af66c5290
derug <78718976+derug@users.noreply.github.com>
2023-03-03 17:57:28 +0900

    Update Manacher.cpp

--- string/Manacher.cpp
+++ string/Manacher.cpp
@@ -1,1 +1,18 @@
-.
+// 각 글자를 중심으로 했을 때 최대 회문 길이 배열
+vector<int> manacher(const string& s){
+    int n=s.size();
+    string t="";
+    for(int i=0; i<n; i++){ t+='.'; t+=s[i]; }
+    t+='.';
+    n=t.size();
+
+    int r=0, p=0;
+    vector<int> A(n);
+    for(int i=0; i<n; i++){
+        if(i<=r) A[i]=min(r-i,A[2*p-i]);
+        while(0<=i-A[i]-1&&i+A[i]+1<n&&t[i-A[i]-1]==t[i+A[i]+1]) A[i]++;
+        if(r<i+A[i]) r=i+A[i], p=i;
+    }
+
+    return A;
+}
