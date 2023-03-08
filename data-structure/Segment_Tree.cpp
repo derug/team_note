ef67404b38da3400528b0e511c5abd57c95ca4ba
derug <78718976+derug@users.noreply.github.com>
2023-03-09 01:54:58 +0900

    Update Segment_Tree.cpp

--- data-structure/Segment_Tree.cpp
+++ data-structure/Segment_Tree.cpp
@@ -13,20 +13,20 @@ struct Seg{
     ll init(int nl, int nr, int node){
         if(nl==nr) return sgt[node]=arr[nl];
         int mid=nl+nr>>2;
-        return sgt[node]=min(init(nl, mid, node*2), init(mid+1, nr, node*2+1));
+        return sgt[node]=min(init(nl,mid,node*2), init(mid+1,nr,node*2+1));
     }
 
     ll update(int idx, ll val, int nl, int nr, int node){
         if(idx<nl||nr<idx) return sgt[node];
         if(nl==nr) return sgt[node]=min(sgt[node], val);
         int mid=nl+nr>>1;
-        return sgt[node]=min(update(idx,val,nl,mid,2*node), update(idx,val,mid+1,nr,2*node+1));
+        return sgt[node]=min(update(idx,val,nl,mid,node*2), update(idx,val,mid+1,nr,node*2+1));
     }
 
     ll query(int l, int r, int nl, int nr, int node){
         if(r<nl||nr<l) return INF;
         if(l<=nl&&nr<=r) return sgt[node];
         int mid=nl+nr>>1;
-        return min(query(l, r, nl, mid, 2*node), query(l, r, mid+1, nr, 2*node+1));
+        return min(query(l,r,nl,mid,node*2), query(l,r,mid+1,nr,node*2+1));
     }
 };
