7879e09d4c5b9a901b9d0eae5ad1c07cf0647d77
derug <78718976+derug@users.noreply.github.com>
2023-03-09 01:47:47 +0900

    Update Lazy_Segement_Tree.cpp

--- data-structure/Lazy_Segement_Tree.cpp
+++ data-structure/Lazy_Segement_Tree.cpp
@@ -1,16 +1,18 @@
 typedef long long ll;
 
-const int MAX;
-struct SegmentTree{
+const ll MAX;
+
+struct LazySeg{
     ll arr[MAX], sgt[4*MAX], lazy[4*MAX];
 
     SegmentTree(){
+        memset(arr,0,sizeof(arr));
         memset(sgt,0,sizeof(sgt));
         memset(lazy,0,sizeof(lazy));
     }
 
-    ll init(int l, int r, int idx){
-        if(l==r) return sgt[idx]=arr[l];
+    ll init(int nl, int nr, int node){
+        if(nl==nr) return sgt[node]=arr[l];
         int mid=(l+r)/2;
         return sgt[idx]=init(l,mid,2*idx)+init(mid+1,r,2*idx+1);
     }
@@ -49,5 +51,31 @@ struct SegmentTree{
         int mid=(l+r)/2;
         return query(nl,nr,l,mid,2*idx)
             +query(nl,nr,mid+1,r,2*idx+1);
+    }
+};
+
+const int MAX, INF;
+
+struct Seg{
+    int arr[MAX], sgt[MAX*4];
+
+    int init(int l, int r, int node){
+        if(l==r) return sgt[node]=arr[l];
+        int mid=l+r>>2;
+        return sgt[node]=min(init(l, mid, node*2), init(mid+1, r, node*2+1));
+    }
+
+    int update(int idx, int val, int nl, int nr, int node){
+        if(idx<nl||nr<idx) return sgt[node];
+        if(nl==nr) return sgt[node]=min(sgt[node], val);
+        int mid=nl+nr>>1;
+        return sgt[node]=min(update(idx,val,nl,mid,2*node), update(idx,val,mid+1,nr,2*node+1));
+    }
+
+    int query(int l, int r, int nl, int nr, int node){
+        if(r<nl||nr<l) return INF;
+        if(l<=nl&&nr<=r) return sgt[node];
+        int mid=nl+nr>>1;
+        return min(query(l, r, nl, mid, 2*node), query(l, r, mid+1, nr, 2*node+1));
     }
 };
