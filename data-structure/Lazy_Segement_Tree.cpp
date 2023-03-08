f4cc1ca09f691c9a17d014052b9a79716f394069
derug <78718976+derug@users.noreply.github.com>
2023-03-09 01:54:09 +0900

    Update Lazy_Segement_Tree.cpp

--- data-structure/Lazy_Segement_Tree.cpp
+++ data-structure/Lazy_Segement_Tree.cpp
@@ -12,70 +12,42 @@ struct LazySeg{
     }
 
     ll init(int nl, int nr, int node){
-        if(nl==nr) return sgt[node]=arr[l];
-        int mid=(l+r)/2;
-        return sgt[idx]=init(l,mid,2*idx)+init(mid+1,r,2*idx+1);
-    }
-
-    void update_lazy(int l, int r, int idx){
-        if(lazy[idx]){
-            sgt[idx]+=(r-l+1)*lazy[idx];
-            if(l!=r){
-                lazy[2*idx]+=lazy[idx];
-                lazy[2*idx+1]+=lazy[idx];
-            }
-            lazy[idx]=0;
-        }
-    }
-
-    ll update(int nl, int nr, int l, int r, ll v, int idx){
-        update_lazy(l,r,idx);
-        if(nr<l||r<nl) return sgt[idx];
-        if(nl<=l&&r<=nr){
-            sgt[idx]+=(r-l+1)*v;
-            if(l!=r){
-                lazy[2*idx]+=v;
-                lazy[2*idx+1]+=v;
-            }
-            return sgt[idx];
-        }
-        int mid=(l+r)/2;
-        return sgt[idx]=update(nl,nr,l,mid,v,2*idx)
-            +update(nl,nr,mid+1,r,v,2*idx+1);
-    }
-
-    ll query(int nl, int nr, int l, int r, int idx){
-        update_lazy(l,r,idx);
-        if(r<nl||nr<l) return 0;
-        if(nl<=l&&r<=nr) return sgt[idx];
-        int mid=(l+r)/2;
-        return query(nl,nr,l,mid,2*idx)
-            +query(nl,nr,mid+1,r,2*idx+1);
-    }
-};
-
-const int MAX, INF;
-
-struct Seg{
-    int arr[MAX], sgt[MAX*4];
-
-    int init(int l, int r, int node){
-        if(l==r) return sgt[node]=arr[l];
-        int mid=l+r>>2;
-        return sgt[node]=min(init(l, mid, node*2), init(mid+1, r, node*2+1));
-    }
-
-    int update(int idx, int val, int nl, int nr, int node){
-        if(idx<nl||nr<idx) return sgt[node];
-        if(nl==nr) return sgt[node]=min(sgt[node], val);
+        if(nl==nr) return sgt[node]=arr[nl];
         int mid=nl+nr>>1;
-        return sgt[node]=min(update(idx,val,nl,mid,2*node), update(idx,val,mid+1,nr,2*node+1));
+        return sgt[node]=init(nl,mid,node*2)+init(mid+1,nr,node*2+1);
     }
 
-    int query(int l, int r, int nl, int nr, int node){
-        if(r<nl||nr<l) return INF;
+    void update_lazy(int nl, int nr, int node){
+        if(lazy[node]){
+            sgt[node]+=(nr-nl+1)*lazy[node];
+            if(l!=r){
+                lazy[node*2]+=lazy[node];
+                lazy[node*2+1]+=lazy[node];
+            }
+            lazy[node]=0;
+        }
+    }
+
+    ll update(int l, int r, ll val, int nl, int nr, int node){
+        update_lazy(nl,nr,node);
+        if(r<nl||nr<l) return sgt[node];
+        if(l<=nl&&nr<=r){
+            sgt[node]+=(nr-nl+1)*val;
+            if(nl!=nr){
+                lazy[node*2]+=val;
+                lazy[node*2+1]+=val;
+            }
+            return sgt[node];
+        }
+        int mid=nl+nr>>1;
+        return sgt[node]=update(l,r,val,nl,mid,node*2)+update(l,r,val,mid+1,nr,node*2+1);
+    }
+
+    ll query(int l, int r, int nl, int nr, int node){
+        update_lazy(nl,nr,node);
+        if(r<nl||nr<l) return 0;
         if(l<=nl&&nr<=r) return sgt[node];
         int mid=nl+nr>>1;
-        return min(query(l, r, nl, mid, 2*node), query(l, r, mid+1, nr, 2*node+1));
+        return query(l,r,nl,mid,node*2)+query(l,r,mid+1,nr,node*2+1);
     }
 };
