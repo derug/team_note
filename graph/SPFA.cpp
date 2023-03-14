fb589dfa91648923c630e7c2bf826e7cc99111c2
derug <78718976+derug@users.noreply.github.com>
2023-03-14 22:01:02 +0900

    Update SPFA.cpp

--- graph/SPFA.cpp
+++ graph/SPFA.cpp
@@ -1,1 +1,33 @@
-.
+const int MAX, INF;
+
+vector<pair<int,int>> adj[MAX]; //(번호, 가중치)
+
+vector<int> SPFA(int n, int start){
+    vector<int> dst(n+1, INF);
+    vector<int> visited(n+1);
+    vector<bool> inQ(n+1);
+    queue<int> Q;
+    
+    Q.push(start);
+    dst[start]=0;
+    ++visited[start];
+    inQ[start]=true;
+    while(!Q.empty()){
+        int idx=Q.front(); Q.pop();
+        inQ[idx]=false;
+        for(auto p: adj[idx]){
+            int next=p.first;
+            int ndst=dst[idx]+p.second;
+            if(ndst<dst[next]){
+                dst[next]=ndst;
+                if(!inQ[next]){
+                    Q.push(next);
+                    inQ[next]=true;
+                    if(++visited[next]>=n)
+                        return vector<int>();
+                }
+            }
+        }
+    }
+    return dst;
+}
