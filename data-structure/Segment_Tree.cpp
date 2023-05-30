1137a7125c4ba73c30911a92617748ebf4cdb800
dhlee1031 <56053565+dhlee1031@users.noreply.github.com>
2023-05-30 12:10:24 +0900

    Update Segment_Tree.cpp

--- data-structure/Segment_Tree.cpp
+++ data-structure/Segment_Tree.cpp
@@ -30,3 +30,27 @@ struct Seg{
         return min(query(l,r,nl,mid,node*2), query(l,r,mid+1,nr,node*2+1));
     }
 };
+
+// ver. 2
+
+ll init(int idx, int s, int e)
+{
+	if (s == e) return T[idx] = a[s];
+	int m = s + e >> 1;
+	return T[idx] = init(idx * 2, s, m) + init(idx * 2 + 1, m + 1, e);
+}
+ll upd(int idx, int s, int e, int t, ll val)
+{
+	if (t < s || e < t) return T[idx];
+	if (s == e) return T[idx] = val;
+	int m = s + e >> 1;
+	return T[idx] = upd(idx * 2, s, m, t, val) + upd(idx * 2 + 1, m + 1, e, t, val);
+}
+ll qry(int idx, int s, int e, int l, int r)
+{
+	if (r < s || e < l) return 0;
+	if (l <= s && e <= r) return T[idx];
+	int m = s + e >> 1;
+	return qry(idx * 2, s, m, l, r) + qry(idx * 2 + 1, m + 1, e, l, r);
+}
+
