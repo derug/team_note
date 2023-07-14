0a32116dd9e2e10181a13baaea790e08d5d23641
derug <78718976+derug@users.noreply.github.com>
2023-07-14 16:44:15 +0900

    KMP.cpp

--- string/KMP.cpp
+++ string/KMP.cpp
@@ -1,28 +1,35 @@
-string p, t;
-vector<int> c;
-vector<int> pos;
+class KMP{
+    string p, t;
+    vector<int> c;
+    vector<int> pos;
 
-void pi(string& s){
-    c.clear();
-    p=s;
-    c.resize(p.size()+1);
-    int k=0;
-    for(int i=1; i<p.size(); i++){
-        while(k>0&&p[k]!=p[i]) k=c[k-1];
-        if(p[k]==p[i]) c[i]=++k;
+    KMP(){this->init();}
+
+    void init(){
+        c.clear();
+        pos.clear();
     }
-}
-
-void find(string& s){
-    pos.clear();
-    t=s;
-    int k=0;
-    for(int i=0; i<t.size(); i++){
-        while(k>0&&t[i]!=p[k]) k=c[k-1];
-        if(t[i]==p[k]){
-            k++;
-            if(k==p.size())
-                pos.push_back(i-p.size()+1);
+    
+    void pi(string& s){
+        p=s;
+        c.resize(p.size()+1);
+        int k=0;
+        for(int i=1; i<p.size(); i++){
+            while(k>0&&p[k]!=p[i]) k=c[k-1];
+            if(p[k]==p[i]) c[i]=++k;
+        }
+    }
+    
+    void find(string& s){
+        t=s;
+        int k=0;
+        for(int i=0; i<t.size(); i++){
+            while(k>0&&t[i]!=p[k]) k=c[k-1];
+            if(t[i]==p[k]){
+                k++;
+                if(k==p.size())
+                    pos.push_back(i-p.size()+1);
+            }
         }
     }
 }
