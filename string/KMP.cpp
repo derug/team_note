1db052cdfea02493f9bb2ac19c7896a1dcc46e2c
derug <78718976+derug@users.noreply.github.com>
2023-07-14 16:42:35 +0900

    KMP.cpp

--- string/KMP.cpp
+++ string/KMP.cpp
@@ -1,1 +1,28 @@
-.
+string p, t;
+vector<int> c;
+vector<int> pos;
+
+void pi(string& s){
+    c.clear();
+    p=s;
+    c.resize(p.size()+1);
+    int k=0;
+    for(int i=1; i<p.size(); i++){
+        while(k>0&&p[k]!=p[i]) k=c[k-1];
+        if(p[k]==p[i]) c[i]=++k;
+    }
+}
+
+void find(string& s){
+    pos.clear();
+    t=s;
+    int k=0;
+    for(int i=0; i<t.size(); i++){
+        while(k>0&&t[i]!=p[k]) k=c[k-1];
+        if(t[i]==p[k]){
+            k++;
+            if(k==p.size())
+                pos.push_back(i-p.size()+1);
+        }
+    }
+}
