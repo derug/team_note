1d0727960235956bae9c78a276ac53b340059a4c
derug <78718976+derug@users.noreply.github.com>
2023-07-18 18:40:26 +0900

    Update Z.cpp

--- string/Z.cpp
+++ string/Z.cpp
@@ -1,1 +1,18 @@
-.
+//
+vector<int> Z(string s){
+    int n=s.size();
+    vector<int> z(n);
+    z[0]=n;
+
+    int l=0, r=0;
+    for(int i=1; i<n; i++){
+        if(z[i-l]<r-i+1) z[i]=z[i-l];
+        else{
+            if(i>r) r=i;
+            l=i;
+            while(r<n&&s[r-l]==s[r]) r++;
+            z[i]=r-l; r--;
+        }
+    }
+    return z;
+}
