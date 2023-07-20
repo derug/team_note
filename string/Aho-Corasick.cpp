53bc75cc0452eb5baf33aec1d938c4f22088d43d
derug <78718976+derug@users.noreply.github.com>
2023-07-20 12:49:00 +0900

    Update Aho-Corasick.cpp

--- string/Aho-Corasick.cpp
+++ string/Aho-Corasick.cpp
@@ -21,45 +21,53 @@ struct Trie{
         }
     }
 
-    void pi(){
-        Trie* root=this;
-        queue<Trie*> Q;
-
-        root->fail=root;
-        Q.push(root);
-        while(!Q.empty()){
-            Trie* cur=Q.front(); Q.pop();
-            for(auto& p : cur->ch){
-                Trie* next=p.second;
-                if(cur==root) next->fail=root;
-                else{
-                    Trie* prev=cur->fail;
-                    while(prev!=root&&prev->ch[p.first]==nullptr)
-                        prev=prev->fail;
-                    if(prev->ch[p.first]!=nullptr)
-                        prev=prev->ch[p.first];
-                    next->fail=prev;
-                }
-                if(next->fail->check)
-                    next->check=true;
-                Q.push(next);
-            }
+    bool find(string& s, int idx){
+        if(idx==s.size()) return check;
+        else{
+            if(ch[s[idx]]==nullptr)
+                return false;
+            return ch[s[idx]]->find(s,idx+1);
         }
-    }
-    
-    vector<int> find(string& s){
-        int n=s.size();
-        Trie* cur=this;
-        vector<int> pos;
-        
-        for(int i=0; i<n; i++){
-            while(cur!=this&&cur->ch[s[i]]==nullptr)
-                cur=cur->fail;
-            if(cur->ch[s[i]]!=nullptr)
-                cur=cur->ch[s[i]];
-            if(cur->check)
-                pos.push_back(i);
-        }
-        return pos;
     }
 };
+
+void pi(Trie* root){
+    queue<Trie*> Q;
+
+    root->fail=root;
+    Q.push(root);
+    while(!Q.empty()){
+        Trie* cur=Q.front(); Q.pop();
+        for(auto& p : cur->ch){
+            Trie* next=p.second;
+            if(cur==root) next->fail=root;
+            else{
+                Trie* prev=cur->fail;
+                while(prev!=root&&prev->ch[p.first]==nullptr)
+                    prev=prev->fail;
+                if(prev->ch[p.first]!=nullptr)
+                    prev=prev->ch[p.first];
+                next->fail=prev;
+            }
+            if(next->fail->check)
+                next->check=true;
+            Q.push(next);
+        }
+    }
+}
+
+vector<int> find(Trie* root, string& s){
+    int n=s.size();
+    Trie* cur=root;
+    vector<int> pos;
+    
+    for(int i=0; i<n; i++){
+        while(cur!=this&&cur->ch[s[i]]==nullptr)
+            cur=cur->fail;
+        if(cur->ch[s[i]]!=nullptr)
+            cur=cur->ch[s[i]];
+        if(cur->check)
+            pos.push_back(i);
+    }
+    return pos;
+}
