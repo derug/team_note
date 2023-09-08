6abdc053f4b2466b322f6adc62fecd25877add7d
derug <78718976+derug@users.noreply.github.com>
2023-09-08 17:41:49 +0900

    Update Strongly_Connected_Component(Tarjan).cpp

--- graph/Strongly_Connected_Component(Tarjan).cpp
+++ graph/Strongly_Connected_Component(Tarjan).cpp
@@ -1,1 +1,50 @@
-.
+const int MAX;
+
+vector<int> adj[MAX];
+
+struct SCC{
+    int n, sn, idx;
+    vector<int> col, par;
+    vector<bool> fin;
+    vector<vector<int>> scc;
+    stack<int> S;
+
+    SCC(int _n): n(_n), sn(0), idx(0){
+        col.resize(n);
+        par.resize(n);
+        fin.resize(n);
+    }
+
+    int dfs(int a){
+        par[a]=++idx;
+        S.push(a);
+        
+        int res=par[a];
+        for(int b: adj[a]){
+            if(!par[b])
+                res=max(res,dfs(b));
+            else if(!fin[b])
+                res=max(res,par[b]);
+        }
+
+        if(res==par[a]){
+            vector<int> tmp;
+            while(1){
+                int t=S.top(); S.pop();
+                tmp.push_back(t);
+                col[t]=sn;
+                fin[t]=true;
+                if(t==a) break;
+            }
+            scc.push_back(tmp);
+            sn++;
+        }
+
+        return res;
+    }
+
+    void makeSCC(){
+        for(int i=0; i<n; i++)
+            if(!par[i]) dfs(i);
+    }
+}
