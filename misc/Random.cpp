9bda9b309886a22c7170458806fee914b85166e4
derug <78718976+derug@users.noreply.github.com>
2025-06-20 15:20:12 +0900

    Update Random.cpp

--- misc/Random.cpp
+++ misc/Random.cpp
@@ -2,4 +2,6 @@ struct Random{
     mt19937 gen;
     Random(){ gen.seed(chrono::steady_clock::now().time_since_epoch().count()); }
     int get(int l, int r){ return uniform_int_distribution<int>(l,r)(gen); }
+    template<typename T>
+    void shuffle(vector<T>& v){ std::shuffle(v.begin(), v.end(), gen); }
 } rnd;
