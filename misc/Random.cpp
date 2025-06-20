8d1adc6b90a58cd56ca12d04edebd90f31cf97d7
derug <78718976+derug@users.noreply.github.com>
2025-06-20 15:12:43 +0900

    Create Random.cpp

---
+++ misc/Random.cpp
@@ -0,0 +1,7 @@
+#include <chrono>
+
+struct Random{
+    mt19937 gen;
+    Random(){ gen.seed(chrono::steady_clock::now().time_since_epoch().count()); }
+    int get(int l, int r){ return uniform_int_distribution<int>(l,r)(gen); }
+} rnd;
